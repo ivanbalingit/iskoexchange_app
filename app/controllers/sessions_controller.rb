41460dade6e80f4a064936ec8197ff9fe0efbbba
Ivan Balingit <ivancarlobalingit@gmail.com>
2018-01-31 04:43:58 +0000

    Add login validation messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:session][:email])
-  	if user.authenticate(params[:session][:password])
+  	if !user
+  	  # If the email does not exist in the database
+  	  @notice = "Email does not exist."
+  	  render action: 'new'
+  	elsif user.authenticate(params[:session][:password])
+  	  # If the email exists and password is correct
   		session[:email] = user.email
+  		redirect_to root_url
+  	else
+  	  # If the email exists but the password is incorrect
+  	  @email = user.email
+  	  @notice = "Invalid password."
+  	  render action: 'new'
   	end
-  	redirect_to '/'
   end
 
   def destroy
