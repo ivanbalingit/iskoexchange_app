ee4e52c74aa5743ae7aecc981f0bc86495556292
Ivan Balingit <ivancarlobalingit@gmail.com>
2018-01-31 04:17:10 +0000

    Create Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      render action: 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:email, :display_name, :password, :password_confirmation)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, :display_name, presence: true
 end
