ab68054f806789e8b861d7e53612731d32e32118
pgcajaljal <patriciamaecajaljal@gmail.com>
2018-02-18 13:10:23 +0800

    Added license

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,31 @@
+=begin
+    Description : Ruby file for schema
+    Copyright (C) 2018  Ivan Balingit
+
+    This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2015-2016
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see https://www.gnu.org/licenses/.
+
+     **CHANGELOG**
+     Ivan Balingit 1/26/18 - Add database credentials
+     Ivan Balingit 1/31/18 - Create User Model
+     Luis Tan 2/2/18 - Added Questions controller
+     Luis Tan 2/14/18 - Add create answer
+
+     File created on: 1/26/18
+     Developer: Ivan Balingit
+     Client: UP Diliman Students
+     IskoExchange is a platform for UP students to ask questions and share insights related to UP
+=end
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
